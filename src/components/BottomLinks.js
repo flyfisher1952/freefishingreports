d2ac47acd619877a52e3d3fa6443651083ac862e
Mike Lockhart <flyfisher1952@gmail.com>
2023-01-09 08:27:18 -0700

    more new stuff

---
+++ src/components/BottomLinks.js
@@ -0,0 +1,17 @@
+import "../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import image0 from "./../images/pexels-azwar-ipank-1133086.jpg";
+
+const BottomLinks = () => {
+    return (
+        <div className="banner-container">
+            <div className="row vh-100">
+                <div className="col-md-3">Links Here</div>
+                <div className="col-md-3">Links Here</div>
+                <div className="col-md-3">Links Here</div>
+            </div>
+        </div>
+    );
+};
+
+export default BottomLinks;
---
+++ src/components/SolunarDay.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-bootstrap/dist/react-bootstrap";
+
+function SolunarDay() {
+    return (
+        <div className="container-fluid">
+            <div className="row bordered-component vh-100">
+                <h1>Solunar Day</h1>
+            </div>
+        </div>
+    );
+}
+
+export default SolunarDay;
