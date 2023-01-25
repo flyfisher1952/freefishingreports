193c2a1a43cd12d6631c3e8fecc981da28bc0b3a
Mike Lockhart <flyfisher1952@gmail.com>
2023-01-25 10:38:48 -0700

    adding typeahead

---
+++ src/components/db-services/getCountries.js
@@ -0,0 +1,18 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
+
+export function getCountries(url) {
+    return new Promise((resolve) => {
+        async function fetchCountries() {
+            const response = await fetch(url);
+            const json = await response.json();
+
+            console.log("---> fetched %d countries", json.length);
+            resolve(json);
+        }
+
+        useEffect(() => {
+            fetchCountries();
+        }, []);
+    });
+}
