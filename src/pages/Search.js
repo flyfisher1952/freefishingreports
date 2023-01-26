ebaee9c77f9ab5a5d2bf8a34dda42b83449dbb79
Mike Lockhart <flyfisher1952@gmail.com>
2023-01-26 11:39:16 -0700

    converted SearchBar to class

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "../components/SearchBar";
 import ReportList from "../components/ReportList";
 import Adds from "../components/Adds";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Search = () => {
+    const [searchFilters, setSearchFilters] = useState({});
+
+    const searchClickHandler = () => {};
+    
     return (
         <div className="container-fluid">
             <div className="row align-items-start">
                 <div className="col-2">
-                    <SearchBar />
+                    <SearchBar onClick={searchClickHandler} />
                 </div>
                 <div className="col-8">
                     <ReportList />
