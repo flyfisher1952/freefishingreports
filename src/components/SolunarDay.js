ad1e4bc384d46cd01e2e39c4c8e1a759b5f19057
Mike Lockhart <flyfisher1952@gmail.com>
2023-02-10 11:17:21 -0700

    web scrapper works, needs styling.

--- src/components/SolunarDay.js
+++ src/components/SolunarDay.js
@@ -1,13 +1,31 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Spinner from "react-bootstrap/Spinner";
-import { UsefetchSolunarDay } from "./hooks/SolunarDayHooks";
+// import { UsefetchSolunarDay } from "./hooks/SolunarDayHooks";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function SolunarDay() {
-    const url = "https://solunarforecast.com/hunting_fishing/best_times/zip_postal_code/calendar/us/84325";
-    const [solunarDay, isLoading] = UsefetchSolunarDay(url);
+const SolunarDay = (props) => {
+    const [solunarDay, setSolunarDay] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
+
+    async function fetchSolunarDay(countryCode, postalCode) {
+        try {
+            setIsLoading(true);
+
+            const response = await fetch("http://localhost:3033/solunarDay/" + countryCode + "/" + postalCode);
+            // const data = await response;
+
+            response.text().then((html) => {
+                setSolunarDay(html);
+                setIsLoading(false);
+            });
+            // setSolunarDay(response.text());
+            // setIsLoading(false);
+        } catch (err) {
+            console.error(err);
+        }
+    }
 
     const LoadingDisplay = () => {
         return (
@@ -26,16 +44,20 @@ function SolunarDay() {
     const LoadedDisplay = () => {
         return (
             <div className="row justify-content-center h-100">
-                <div className="col hidden-md-down text-center">{solunarDay}</div>
+                <div className="col hidden-md-down text-center" dangerouslySetInnerHTML={{ __html: solunarDay }}></div>
             </div>
         );
     };
+
+    useEffect(() => {
+        fetchSolunarDay();
+    }, []);
 
     return (
         <div className="container-fluid h-100">
             <div className="row bordered-component">{isLoading ? <LoadingDisplay /> : <LoadedDisplay />}</div>
         </div>
     );
-}
+};
 
 export default SolunarDay;
--- src/components/hooks/SolunarDayHooks.js
+++ src/components/hooks/SolunarDayHooks.js
@@ -1,37 +1,16 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
-import * as cheerio from "cheerio";
 
 const UsefetchSolunarDay = (url) => {
     const [data, setData] = useState({});
     const [isLoading, setIsLoading] = useState(true);
 
-    function padTo2Digits(num) {
-        return num.toString().padStart(2, "0");
-    }
-
-    function formatDate(date) {
-        let dateString = [padTo2Digits(date.getMonth() + 1), padTo2Digits(date.getDate()), date.getFullYear().toString().substring(2)].join("/");
-
-        return dateString;
-    }
-
-    // Async function which scrapes the data
     async function fetchSolunarDay() {
         try {
             setIsLoading(true);
 
-            const today = formatDate(new Date());
-            const { data } = await axios.get(url);
+            const response = await fetch(url);
 
-            console.log("---> got page: " + data);
-
-            const $ = cheerio.load(data);
-            const dayCell = $("div.panel h4:contains('" + today + "')");
-
-            console.log("---> got DIV: " + dayCell);
-
-            setData(dayCell.parent().parent());
+            setData(response.data);
             setIsLoading(false);
         } catch (err) {
             console.error(err);
@@ -39,9 +18,7 @@ const UsefetchSolunarDay = (url) => {
     }
 
     useEffect(() => {
-        console.log("---> fetching Solunar Day page ");
         fetchSolunarDay();
-        console.log("---> fetched Solunar Day page ");
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
