652ede47193297601edbe8353fcb0c062aeefc77
Mike Lockhart <flyfisher1952@gmail.com>
2023-02-20 12:04:31 -0700

    creates user; needs error checking

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -12,15 +12,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "react-bootstrap-typeahead/css/Typeahead.css";
 
 const Register = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [showPassword, setShowPassword] = useState(false);
-    const [email, setEmail] = useState("");
-    const [postalCode, setPostalCode] = useState("");
-    const [buttonDisabled, setButtonDisabled] = useState(true);
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
     const [dbCountries, setDbCountries] = useState([]);
-    const [selectedCountry, setSelectedCountry] = useState({});
+    const [email, setEmail] = useState("");
     const [isLoadingCountries, setIsLoadingCountries] = useState(true);
+    const [isValidEmail, setIsValidEmail] = useState(true);
+    const [isValidPassword, setIsValidPassword] = useState(true);
+    const [isValidPostalCode, setIsValidPostalCode] = useState(true);
+    const [isValidUsername, setIsValidUsername] = useState(true);
+    const [password, setPassword] = useState("");
+    const [postalCode, setPostalCode] = useState("");
+    const [selectedCountry, setSelectedCountry] = useState({});
+    const [showPassword, setShowPassword] = useState(false);
+    const [username, setUsername] = useState("");
+
+    const usernameRe = new RegExp("/^[a-zA-Z]+[\\w\\d_\\-\\.]*$/");
+    const emailRe = new RegExp("([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})");
 
     async function getCountries() {
         setIsLoadingCountries(true);
@@ -36,20 +43,95 @@ const Register = () => {
         setShowPassword(!showPassword);
     };
 
-    const isValidEmail = () => {};
+    const validatePassword = (pw) => {
+        let isValid = pw.length >= 8;
+        setIsValidPassword(isValid);
 
-    const enableButton = () => {
-        let isDisabled = !(username.length >= 4 && password.length >= 8 && isValidEmail(email) && postalCode.length > 4);
-
-        setButtonDisabled(isDisabled);
+        return isValid;
     };
 
-    const register = () => {};
+    const validateEmail = (em) => {
+        let isValid = emailRe.test(em);
+        console.log("---> validateEmail.emailRe.test(email): " + emailRe.test(em));
+
+        setIsValidEmail(isValid);
+        return isValid;
+    };
+
+    const validateUsername = (un) => {
+        let isValid = true;
+        if (un.length >= 6 && usernameRe.test(un)) {
+            async function fetchUsertname(un) {
+                try {
+                    const response = await fetch("http://localhost:3033/user/name/" + un);
+                    const user = await response.json();
+
+                    console.log("---> validateUsername.response.json: " + JSON.stringify(user));
+                    isValid = !Object.hasOwn(user, "username");
+                    setIsValidUsername(isValid);
+                } catch (err) {
+                    console.error(err);
+                    isValid = false;
+                    setIsValidUsername(isValid);
+                }
+            }
+        }
+
+        return isValid;
+    };
+
+    const validatePostalCode = (pc) => {
+        let isValid = postalCode.length > 4;
+
+        setIsValidPostalCode(isValid);
+        return isValid;
+    };
+
+    const enableButton = () => {
+        let isDisabled = !(isValidUsername && isValidPassword && isValidEmail && Object.hasOwn(selectedCountry, "country") && isValidPostalCode);
+
+        setIsButtonDisabled(isDisabled);
+    };
+
+    const register = async () => {
+        const user = {
+            username: username,
+            email: email,
+            country_id: selectedCountry.country[0].id,
+            postal_code: postalCode,
+            password: password,
+        };
+        const body = JSON.stringify(user);
+        console.log("--------------------------------------------------------------------");
+        console.log("---> selectedCountry: " + JSON.stringify(selectedCountry));
+        console.log("---> country_id: " + selectedCountry.country[0].id);
+        console.log("---> body: " + body);
+        const response = await fetch("http://localhost:3033/user", {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: body,
+        });
+        const responseObject = await response.json();
+
+        // if (response.status === 200 && responseObject.affectedRows === 1) {
+        //     setStatus("Report Submitted successfully.");
+        // } else {
+        //     setStatus("Report Submition Failed.");
+        // }
+    };
 
     useEffect(() => {
+        validateEmail(email);
+        validateUsername(username);
+        validatePassword(password);
+        validatePostalCode(postalCode);
         enableButton();
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [username, password, email, postalCode]);
+    }, [username, password, email, selectedCountry, postalCode]);
 
     return (
         <div className="container-fluid">
@@ -66,11 +148,14 @@ const Register = () => {
                                 </td>
                                 <td>
                                     <input
-                                        className="form-control user-input"
+                                        className={"form-control user-input" + !{ isValidUsername } ? " invalid-input" : ""}
                                         type="text"
                                         value={username}
                                         onChange={(e) => {
                                             setUsername(e.target.value);
+                                        }}
+                                        onBlur={(e) => {
+                                            validateUsername(username);
                                         }}
                                     ></input>
                                 </td>
@@ -83,7 +168,7 @@ const Register = () => {
                                     <h6>Password: </h6>
                                 </td>
                                 <td>
-                                <input
+                                    <input
                                         className="form-control user-input"
                                         type="password"
                                         value={password}
@@ -102,11 +187,14 @@ const Register = () => {
                                 </td>
                                 <td>
                                     <input
-                                        className="form-control user-input"
+                                        className={"form-control user-input" + !{ isValidEmail } ? " invalid-input" : ""}
                                         type="text"
                                         value={email}
                                         onChange={(e) => {
                                             setEmail(e.target.value);
+                                        }}
+                                        onBlur={(e) => {
+                                            validateEmail(email);
                                         }}
                                         data-lpignore="true"
                                     ></input>
@@ -159,7 +247,7 @@ const Register = () => {
                             <tr>
                                 <td>&nbsp;</td>
                                 <td className="float-right">
-                                    <Button variant="success" disabled={buttonDisabled} onClick={register}>
+                                    <Button variant="success" disabled={isButtonDisabled} onClick={register}>
                                         Register
                                     </Button>
                                 </td>
